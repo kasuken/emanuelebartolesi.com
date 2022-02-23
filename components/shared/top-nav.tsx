625ae7d3b126a8791c155c3f428d853d904b379f
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-02-23 09:58:36 +0100

    remove temporary links

--- components/shared/top-nav.tsx
+++ components/shared/top-nav.tsx
@@ -45,9 +45,9 @@ const mobileLinks = [
 
 const dropdownLinks = [
   { name: 'Projects', path: '/projects' },
-  { name: 'Tech Stack', path: '/tech-stack' },
+  // { name: 'Tech Stack', path: '/tech-stack' },
   { name: 'Open Source', path: '/open-source' },
-  { name: 'Achievements', path: '/achievements' },
+  // { name: 'Achievements', path: '/achievements' },
   { name: 'Changelog', path: '/changelog' },
 ];
 

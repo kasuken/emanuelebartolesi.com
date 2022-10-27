4bd54d9895c3e28ccb74bdb6935bb1590e1add07
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-10-27 17:44:26 +0200

    change url for dev.to posts

--- lib/fetchPosts.ts
+++ lib/fetchPosts.ts
@@ -1,7 +1,8 @@
 import useSWR from 'swr'
 
 export const getDbPosts = () => {
-  const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_DEVTO_API_URL}/articles?username=${process.env.NEXT_PUBLIC_DEVTO_USERNAME}`, getDevtoPosts);
+  //const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_DEVTO_API_URL}/articles?username=${process.env.NEXT_PUBLIC_DEVTO_USERNAME}`, getDevtoPosts);
+  const { data, error } = useSWR(`https://dev.to/api/articles?username=kasuken`, getDevtoPosts);
 
   return {
     dbPosts: data?.dbPosts,
@@ -11,7 +12,8 @@ export const getDbPosts = () => {
 }
 
 export const getDevtoPosts = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_DEVTO_API_URL}/articles?username=${process.env.NEXT_PUBLIC_DEVTO_USERNAME}`);
+  //const res = await fetch(`${process.env.NEXT_PUBLIC_DEVTO_API_URL}/articles?username=${process.env.NEXT_PUBLIC_DEVTO_USERNAME}`);
+  const res = await fetch(`https://dev.to/api/articles?username=kasuken`);
 
   if (res.status < 200 || res.status >= 300) {
     throw new Error(
